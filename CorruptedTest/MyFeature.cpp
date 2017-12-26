ba28b9cb6b1abbf6ce739ea50ea3f5335fac90ad
Franck Royer <royer.franck@gmail.com>
2017-12-26 10:59:59 +0000

    update with more enticing logic

--- CorruptedTest/MyFeature.cpp
+++ CorruptedTest/MyFeature.cpp
@@ -4,9 +4,10 @@ const std::string MyFeature::execute(const MyDataClass&  iData)
 {
     std::string str = "Merry Christmas! ";
     const int aInt = iData.getInt();
-    for(std::size_t i = 0; i < aInt+1; ++i)
+    if (aInt)
     {
-        str.append(iData.getString());
+        str = "Creepy Halloween! ";
     }
+    str.append(iData.getString());
     return str;
 }
